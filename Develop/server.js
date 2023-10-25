88d15d00cf20101d87f54667549018b18533136c
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-10-25 00:49:08 -0500

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -13,17 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
     console.log('get /api/notes ')
-    res.status(200).json(`${req.method} request received to get notes`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 app.post('/api/notes', (req, res) => {
@@ -44,7 +46,6 @@ app.post('/api/notes', (req, res) => {
                 const parsedNotes = JSON.parse(data);
 
                 parsedNotes.push(newNote);
-
 
                 fs.writeFile(
                     './db/db.json',
