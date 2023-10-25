312a4908a866f35849e790f0e2cb705094c7a0e7
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-10-24 20:29:42 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path')
 const noteData = require('./db/db.json');
+const fs = require('fs');
 const uuid = require('./helpers/uuid.js');
-const notes = require('./public/notes.html');
 
 const app = express();
 
 const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -18,17 +21,19 @@ app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-app.get('api/notes', (req,res) => {
-    res.json(noteData)
+app.get('/api/notes', (req,res) => {
+    console.log('get /api/notes ')
+    res.status(200).json(noteData);
 });
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request recieved to save note.`);
     const { title, text } = req.body
     if (title && text) {
         const newNote = {
             title, 
-            text
+            text, 
+            note_id: uuid()
         };
 
         const response = {
