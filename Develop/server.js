850aa35da1f5428fee8529d086f147ae3f9ce9bf
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-10-25 00:26:23 -0500

    updated server.js,index.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path')
-const noteData = require('./db/db.json');
+
 const fs = require('fs');
 const uuid = require('./helpers/uuid.js');
 
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
